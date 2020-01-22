e0785ab37aafe928a5289f9737c33cd1adce4eb5
swyx <shawnthe1@gmail.com>
2020-01-22 11:35:29 -0500

    functions

--- .functions/alexa1/alexa1.js
+++ .functions/alexa1/alexa1.js
@@ -1,32 +1,30 @@
-/*
 exports.handler = async (event, context) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
+    const subject = event.queryStringParameters.name || 'World';
     return {
       statusCode: 200,
       body: JSON.stringify({ message: `Hello ${subject}` })
       // // more keys you can return:
       // headers: { "headerName": "headerValue", ... },
       // isBase64Encoded: true,
-    }
+    };
   } catch (err) {
-    return { statusCode: 500, body: err.toString() }
+    return { statusCode: 500, body: err.toString() };
   }
-}
-*/
+};
 
-exports.handler = function(event, context, callback) {
-  console.log('work please');
-	/*
-	Alexa info is in event.body. It's a JSON packet contaning a lot of
-	information with the primary thing we want being intent
-	*/
-	let intent = '';
-	if(event.body.request && event.body.intent) intent = event.body.intent.name;
-	console.log(`intent=${intent}`);
-	callback(null, {
-    statusCode:200,
-    body: 'sent '+intent + ' at ' + (new Date())
-	});
-	
-}
+// exports.handler = function(event, context, callback) {
+//   console.log('work please');
+// 	/*
+// 	Alexa info is in event.body. It's a JSON packet contaning a lot of
+// 	information with the primary thing we want being intent
+// 	*/
+// 	let intent = '';
+// 	if(event.body.request && event.body.intent) intent = event.body.intent.name;
+// 	console.log(`intent=${intent}`);
+// 	callback(null, {
+//     statusCode:200,
+//     body: 'sent '+intent + ' at ' + (new Date())
+// 	});
+
+// }
